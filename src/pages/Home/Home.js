0ddfb8e9bcdc9c88c78f38f00b8d2c1fa3c43080
R7558 <rupsanaworld1004@gmail.co>
2023-06-09 01:08:46 +0600

    added header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Header from '../../components/Header/Header';
 
 const Home = () => {
     return (
         <div>
         <Navbar />
-        {/* <Header/>
-        <div className="homeContainer">
+         <Header/>
+        {/* <div className="homeContainer">
           <Featured/>
           <h1 className="homeTitle">Browse by property type</h1>
           <PropertyList/>
           <h1 className="homeTitle">Homes guests love</h1>
           <FeaturedProperties/>
           <MailList/>
-          <Footer/> */}
-        {/* </div> */}
+          <Footer/> 
+        </div> */}
       </div>
     );
 };
