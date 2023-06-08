8b8e3757efe0e3ccc6f60c20f5e28e7e2b8d1d8d
R7558 <rupsanaworld1004@gmail.co>
2023-06-09 00:54:13 +0600

    added navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home</h1>
-        </div>
+        <Navbar />
+        {/* <Header/>
+        <div className="homeContainer">
+          <Featured/>
+          <h1 className="homeTitle">Browse by property type</h1>
+          <PropertyList/>
+          <h1 className="homeTitle">Homes guests love</h1>
+          <FeaturedProperties/>
+          <MailList/>
+          <Footer/> */}
+        {/* </div> */}
+      </div>
     );
 };
 
